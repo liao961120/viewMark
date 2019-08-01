81290ae214e4eb263fb9eae0b0575eb15f3aac44
liao <liao961120@gmail.com>
2019-08-02 00:16:42 +0800

    deploy

--- vue.config.js
+++ vue.config.js
@@ -2,12 +2,4 @@
 module.exports = {
     // options...
     publicPath: './',
-    // disable hashes in filenames
-    filenameHashing: false,
-    // delete HTML related webpack plugins
-    chainWebpack: config => {
-        config.plugins.delete('html')
-        config.plugins.delete('preload')
-        config.plugins.delete('prefetch')
-    }
 }
