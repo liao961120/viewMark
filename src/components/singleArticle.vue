72aeae2f901ff46081e669ea9f6f89a420f37572
liao <liao961120@gmail.com>
2019-08-04 00:25:14 +0800

    Article reading and management

---
+++ src/components/singleArticle.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h2>{{ article.title }}</h2>
+    <p>{{ article.date | toDate }}</p>
+    <ul class="tags">
+      <li v-for="tag in article.tags" v-bind:key="tag.id" class="tag">{{ tag }}</li>
+    </ul>
+    <div class="content">{{ article.content }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    article: Object
+  },
+
+  data() {
+    return {
+      id: parseInt(this.$route.params.id),
+      idIsValid: true
+      //articles: [],
+    };
+  },
+
+  methods: {},
+
+  filters: {
+    toDate: function(value) {
+      var date = new Date(value);
+      return date.toISOString().substr(0, 10);
+    }
+  },
+
+  computed: {},
+
+  created() {}
+};
+</script>
