ce7668d1b4a09e09786e24cc9e0399d3c7ff6798
liao <liao961120@gmail.com>
2019-08-04 00:26:34 +0800

    route for article viewer/management

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
 import mdEditor from "./components/mdEditor.vue"
 import mathEditor from "./components/mathEditor.vue"
 import registerSnippets from "./components/registerSnippets.vue"
+import articleViewer from "./components/viewer.vue";
+
 
 export default [
     {path: '/', component: mdEditor},
     {path: '/math', component: mathEditor},
     {path: '/snippets', component: registerSnippets},
+    {path: '/viewer', component: articleViewer},
 ]
