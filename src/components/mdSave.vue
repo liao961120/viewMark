da5c9b6488f90870954cd6db8f60652ef4823a28
liao <liao961120@gmail.com>
2019-08-04 00:26:05 +0800

    Use modal to save/load markdown article

--- src/components/mdSave.vue
+++ src/components/mdSave.vue
@@ -1,17 +1,57 @@
 <template>
-  <div action>
-    <input type="text" id="title" v-model.lazy="title" required placeholder="Title" />
-    <button v-on:click="saveArticle" v-bind:disabled="mdInputSaved">Save</button>
-    <select v-model="toLoad">
-      <option v-for="(article, idx) in articles" v-bind:key="article.id" v-bind:value="idx">
-        <code>{{ article.date | toDate }}</code>
-        {{ article.title }}
-      </option>
-    </select>
-    <button v-on:click="loadArticle(idx)">Load Article</button>
-    <!-- <button v-on:click="saveArticle">save</button> -->
-    <br />
-
+  <div class="modal">
+    <div class="modal-content">
+      <div class="save">
+        <h3>Save article</h3>
+        <ul>
+          <li>
+            <input
+              type="text"
+              id="title"
+              v-model.lazy="newArticle.title"
+              required
+              placeholder="Title"
+            />
+            <span class="preview">
+              <b>{{ this.newArticle.title }}</b>
+            </span>
+          </li>
+          <li>
+            <input type="text" placeholder="Tag" v-model="tag" />
+            <button v-on:click="addTag">Add</button>
+            <ul class="inline-list preview">
+              <li v-for="tag in newArticle.tags" v-bind:key="tag.id">{{ tag }}&nbsp;</li>
+            </ul>
+          </li>
+          <li class="submit">
+            <button v-on:click="saveArticle" v-bind:disabled="mdInputSaved">Save</button>
+            <span v-if="mdInputSaved" class="message">Article saved!</span>
+          </li>
+        </ul>
+      </div>
+      <div class="load">
+        <h3>Load article</h3>
+        <ul>
+          <li>
+            <select v-model="toLoad">
+              <option
+                v-for="(article, idx) in articles"
+                v-bind:key="article.id"
+                v-bind:value="idx"
+              >{{ article.title }}</option>
+            </select>
+            <span v-if="articles[toLoad]" class="preview article-title">
+              <b>{{ articles[toLoad].title }}</b> &nbsp;
+              <code>{{ articles[toLoad].date | toDate }}</code>
+            </span>
+          </li>
+          <li>
+            <button v-on:click="loadArticle" class="submit">Load</button>
+          </li>
+        </ul>
+      </div>
+      <br />
+    </div>
   </div>
 </template>
 
@@ -23,6 +63,7 @@ ToDo:
 
 // Event bus
 import { bus } from "../main";
+import utils from "./utils";
 
 export default {
   props: {
@@ -35,44 +76,65 @@ export default {
       mdInputSaved: false,
       articles: [],
       toLoad: 0,
-      title: ""
+      tag: "",
+      newArticle: {
+        date: 0,
+        title: "",
+        tags: [],
+        content: ""
+      }
     };
   },
-  methods: {
-    saveArticle: function() {
 
+  watch: {
+    tag: function() {
+      let tag = this.tag;
+
+      if (tag.match(/\s+/)) {
+        this.tag = tag.replace(/\s+/, "");
+      }
+    }
+  },
+
+  methods: {
+    addTag: function() {
+      this.newArticle.tags.push(this.tag);
+      this.tag = "";
+    },
+
+    saveArticle: function() {
       // validity check
-      if (!this.title) return;
+      if (!this.newArticle.title) return;
       if (!this.mdInput) return;
-      
+
       // Check title conflict
       let savedTitles = [];
       let updateIdx = null;
-      this.articles.forEach(ele => {savedTitles.push(ele.title);});
-      if (savedTitles.includes(this.title)) {
-          updateIdx = savedTitles.indexOf(this.title);
-          var keepOld = !confirm('Article exists! Overwrite?');
-          if (keepOld) return
+      this.articles.forEach(ele => {
+        savedTitles.push(ele.title);
+      });
+
+      if (savedTitles.includes(this.newArticle.title)) {
+        updateIdx = savedTitles.indexOf(this.newArticle.title);
+        var keepOld = !confirm("Article exists! Overwrite?");
+        if (keepOld) return;
       }
 
       // Create article obj
-      var now = new Date().getTime();
-      var article = {
-        date: now,
-        title: this.title,
-        content: this.mdInput
-      };
+      this.newArticle.date = new Date().getTime();
+      this.newArticle.content = this.mdInput;
 
       // Add new article
-      if (updateIdx)
-        this.articles[updateIdx] = article;
-      else 
-        this.articles.push(article);
+      var article = utils.deepCopy(this.newArticle);
+      if (updateIdx) this.articles[updateIdx] = article;
+      else this.articles.push(article);
+
       localStorage.setItem("md-articles", JSON.stringify(this.articles));
 
       // Update status
       bus.$emit("mdInputSaved", true);
-      this.title = '';
+      this.newArticle.title = "";
+      this.newArticle.tags = [];
     },
 
     removeArticle: function(idx) {
@@ -83,13 +145,14 @@ export default {
     loadArticle: function() {
       if (this.mdInputSaved)
         bus.$emit("loadArticle", this.articles[this.toLoad].content);
-      else 
-        alert('Markdown content not saved!')
+      else if (confirm("Markdown content not saved!"))
+        bus.$emit("loadArticle", this.articles[this.toLoad].content);
+
       // Cleaup
-      this.title = '';
+      this.newArticle.title = "";
+      this.newArticle.tags = [];
     }
   },
-
 
   mounted() {
     if (localStorage.getItem("md-articles")) {
@@ -116,6 +179,96 @@ export default {
 </script>
 
 <style scoped>
+.inline-list,
+.inline-list li {
+  display: inline;
+  font-size: 0.85em;
+  font-style: italic;
+}
+
+select {
+  max-width: 35%;
+}
+
+.preview {
+  float: right;
+}
+
+.preview.article-title {
+  float: none;
+  display: block;
+  height: 1.2em;
+}
+
+.preview code {
+  font-size: 0.7em;
+}
+
+.submit {
+  display: block;
+  margin: 15px 0;
+}
+
+.modal-content ul {
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+}
+
+.modal-content h3 {
+  margin: 10px 0;
+}
+
+.modal-content > div {
+  margin: 5px 0 20px;
+}
+
+.modal {
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  opacity: 0;
+  visibility: hidden;
+  transform: scale(1.1);
+  transition: visibility 0s linear 0.25s, opacity 0.25s 0s, transform 0.25s;
+  z-index: 100;
+}
+
+.modal-content {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: white;
+  padding: 1rem 1.5rem;
+  width: 24rem;
+  border-radius: 0.5rem;
+}
+
+.close-button {
+  float: right;
+  width: 4em;
+  line-height: 1.5rem;
+  text-align: center;
+  cursor: pointer;
+  border-radius: 0.25rem;
+  background-color: lightgray;
+}
+
+.close-button:hover {
+  background-color: darkgray;
+}
+
+.show-modal {
+  opacity: 1;
+  visibility: visible;
+  transform: scale(1);
+  transition: visibility 0s linear 0s, opacity 0.25s 0s, transform 0.25s;
+  z-index: 100;
+}
 </style>
 
 
