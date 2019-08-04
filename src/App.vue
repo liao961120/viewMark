daab31222258441ee8db7c617ec231dfab32b70d
liao <liao961120@gmail.com>
2019-08-05 00:30:16 +0800

    refactor css

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,25 @@
 <template>
   <div id="app">
     <app-nav></app-nav>
-    <!-- <math-editor></math-editor> -->
     <keep-alive>
       <router-view></router-view>
     </keep-alive>
-    <!-- <md-editor></md-editor> -->
   </div>
 </template>
 
 <script>
-//import mdEditor from "./components/mdEditor";
-//import mathEditor from "./components/mathEditor";
-//import registerSnippets from "./components/registerSnippets";
 import nav from "./components/nav";
 
 export default {
   components: {
     "app-nav": nav
-    //"register-snippets": registerSnippets,
-    //"md-editor": mdEditor,
-    //"math-editor": mathEditor
   }
 };
 </script>
 
 <style>
-@import './assets/variables.css';
-/* @import './assets/modal.css'; */
+@import "./assets/variables.css";
+@import "./assets/print.css";
 
 html {
   height: 100%;
@@ -39,12 +31,92 @@ body {
   font-family: "Roboto", Helvetica, "open sans", sans-serif;
 }
 
-nav a {
-  font-weight: bold;
-  font-family: "Alegreya", Georgia, serif;
-  letter-spacing: 0.05em;
-  font-size: 0.9em;
+a {
+  cursor: pointer;
+  color: #002EFE;
+  text-decoration: underline;
 }
+a:hover {
+  text-decoration: none;
+}
+
+/*/////////////// Footer start /////////////////*/
+footer {
+  background: var(--theme-dark);
+  padding: 2px 3px 5px;
+  margin-bottom: 0px;
+  width: 100%;
+  min-height: 20px;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  z-index: 100;
+  display: grid;
+  grid-template-columns: 1fr 4fr 1fr;
+}
+footer ul {
+  list-style-type: none;
+  text-align: center;
+  margin: 0px 0;
+  padding: 0;
+}
+footer li {
+  display: inline-block;
+  margin: 0 3px;
+  padding: 3.5px 0px;
+}
+footer a {
+  font-size: 0.75em;
+  text-decoration: none;
+  padding: 4px 6px;
+  border-radius: 10px;
+  background: var(--btn-inactive);
+  color: var(--theme-dark);
+}
+footer a:hover {
+  background: var(--theme-light);
+  color: var(--theme-dark);
+}
+footer a.left:hover,
+footer .btn-active,
+footer .btn-full-screen,
+footer .btn-full-screen:hover {
+  background: var(--theme-light);
+  color: var(--button-on);
+}
+footer .left,
+footer .right {
+  align-self: end;
+  /* margin: 1.5px 3px; */
+}
+footer .left {
+  justify-self: start;
+}
+footer .right {
+  justify-self: end;
+}
+footer .left li,
+footer .right li {
+  /* margin: 4px;
+  padding: 2px; */
+}
+footer .left a,
+footer .right a {
+  font-weight: bold;
+}
+/* single article style */
+footer .back::before {
+  content: "\00ab ";
+  display: inline-block;
+  width: 0;
+  opacity: 0;
+  transition: 0.4s;
+}
+footer .back:hover::before {
+  opacity: 1;
+  width: 0.8em;
+}
+/*//////////////// Footer end ////////////////////*/
 
 .outer {
   position: relative;
@@ -93,19 +165,16 @@ span.CodeMirror-selectedtext {
 ::-webkit-scrollbar {
   width: 10px;
 }
-
 /* Track */
 ::-webkit-scrollbar-track {
   box-shadow: inset 0 0 3px grey;
   border-radius: 6px;
 }
-
 /* Handle */
 ::-webkit-scrollbar-thumb {
   background: rgb(144, 144, 144);
   border-radius: 6px;
 }
-
 /* Handle on hover */
 ::-webkit-scrollbar-thumb:hover {
   background: rgb(98, 98, 98);
@@ -121,7 +190,6 @@ span.CodeMirror-selectedtext {
     /* Super Modern Browsers */
       url("./assets/fonts/alegreya-v13-latin-regular.woff") format("woff"); /* Modern Browsers */
 }
-
 /* alegreya-italic - latin */
 @font-face {
   font-family: "Alegreya";
