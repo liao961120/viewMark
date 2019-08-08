f22c5b5dbf0e1037c1bb2c2b1753bba59d5d368f
liao <liao961120@gmail.com>
2019-08-08 11:19:55 +0800

    add syntax.vue route

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,15 @@
 import mdEditor from "./components/mdEditor.vue"
 import mathEditor from "./components/mathEditor.vue"
 import registerSnippets from "./components/registerSnippets.vue"
-import articleViewer from "./components/viewer.vue";
-import mediaPrint from "./components/mediaPrint.vue";
-
+import articleViewer from "./components/viewer.vue"
+import mediaPrint from "./components/mediaPrint.vue"
+import addSyntax from "./components/syntax.vue"
 
 export default [
     {path: '/', component: mdEditor},
     {path: '/math', component: mathEditor},
     {path: '/snippets', component: registerSnippets},
     {path: '/reader', component: articleViewer},
-    {path: '/print', component: mediaPrint}
+    {path: '/print', component: mediaPrint},
+    {path: '/syntax', component: addSyntax},
 ]
